3db207067a6bcd16147ed4098dce34d85d71084e
Jonathon Ho <hello@jonathonho.com>
2015-03-30 21:13:02 +0900

    Updated readme, removed unused code.

--- Assets/Scripts/Player/ArrowController.cs
+++ Assets/Scripts/Player/ArrowController.cs
@@ -10,19 +10,8 @@ public class ArrowController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         //Rotate the Arrow towards the mouse
-        /*
-        Vector3 mousePos = Input.mousePosition;
-        Vector3 objPosition = Camera.main.WorldToScreenPoint(transform.position);
-
-        mousePos.z = 10.0f; //Distance between Mouse and Arrow
-        mousePos.x = mousePos.x - transform.position.x;
-        mousePos.y = mousePos.y - transform.position.y;
-        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
-
-        transform.rotation = Quaternion.Euler(0, 0,angle);
-         */
-
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
         // Rotate object towards mouse position
         Quaternion rot = Quaternion.LookRotation(mousePosition - transform.position, transform.TransformDirection(Vector3.forward));
         transform.rotation = new Quaternion(0, 0, rot.z, rot.w);
